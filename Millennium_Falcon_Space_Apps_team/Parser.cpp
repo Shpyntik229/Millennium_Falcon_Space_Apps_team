20aae79aa869f07c79044509306fcc552dbb663d
Lutsiuk93 <lutsiuk.maksym@lll.kpi.ua>
2021-10-02 15:14:46 +0300

    .

--- Millennium_Falcon_Space_Apps_team/Parser.cpp
+++ Millennium_Falcon_Space_Apps_team/Parser.cpp
@@ -19,7 +19,6 @@ TriangleMesh Parse(const char* filename)
     std::string line;
     while (std::getline(in, line))
     {
-        //check v for vertices
         if (line.substr(0, 2) == "v ") {
             std::istringstream v(line.substr(2));
             double x, y, z;
@@ -27,16 +26,13 @@ TriangleMesh Parse(const char* filename)
             vertices.push_back({ x, y, z });
         }
         else if (line.substr(0, 2) == "f ") {
-            std::size_t i1, i2, i3, i4; //to store mesh index
-            //std::istringstream v;
-          //v.str(line.substr(2));
-            const char* chh = line.c_str();
-            sscanf_s(chh, "f %i/%*i/%*i %i/%*i/%*i %i/%*i/%*i %i/%*i/%*i", &i1, &i2, &i3, &i4); //here it read the line start with f and store the corresponding values in the variables
+            std::size_t i1, i2, i3, i4;
 
-            //v>>a;v>>b;v>>c;
+            const char* chh = line.c_str();
+            sscanf_s(chh, "f %i/%*i/%*i %i/%*i/%*i %i/%*i/%*i %i/%*i/%*i", &i1, &i2, &i3, &i4);
+
             i1--; i2--; i3--; i4--;
 
-            //std::cout<<a<<b<<c<<A<<B<<C;
             triangles.push_back({ i1, i2, i3 });
             triangles.push_back({ i1, i3, i4 });
         }
